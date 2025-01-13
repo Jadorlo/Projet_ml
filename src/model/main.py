14778fa928051a9ab4a8625f981657f986a65752
Jadorlo <alexander.lunel.etu@univ-lille.fr>
2025-01-13 09:39:44 +0100

    ajout du tensorflow dans le gros notebook

--- src/model/main.py
+++ src/model/main.py
@@ -8,7 +8,7 @@ def main():
     N_EPOCHS = 100
 
     model_name = "Close_price"
-    data_name = "dataset_raw_1d"
+    data_name = "dataset_raw_6h"
     full_model_name = model_name+'-'+data_name+'-'+str(N_EPOCHS)
     (date_train, X_train, y_train, date_val, X_val, y_val, date_test, X_test, y_test), scaler_features, scaler_target = main_pre_processing(data_name, is_one_var=True)
     main_training_model(full_model_name, scaler_features, scaler_target, X_train, y_train, X_val, y_val, N_EPOCHS)
